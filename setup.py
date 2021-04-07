aabcff8840cc1eb6f435d4f9c9f8e429aff11526
Anthony De <>
2021-04-07 12:07:47 +0000

    another

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='superofficesdk',
     packages=find_packages(),
-    version='1.0.5',
+    version='1.0.6',
     description='SuperOffice Python SDK',
+    package_data={'superofficesdk': ['PartnerSystemUserService.wsdl']},
+    include_package_data=True, 
     author='Anthony De Smet',
     license='MIT'
 )
