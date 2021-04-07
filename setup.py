91e0dd55edddece4e2f4f13f78ccc55b2f48b4cd
Anthony De <>
2021-04-07 09:48:32 +0000

    upped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='superofficesdk',
     packages=find_packages(),
-    version='1.0.1',
+    version='1.0.2',
     description='SuperOffice Python SDK',
     author='Anthony De Smet',
     license='MIT'
